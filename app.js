8396626c647635719b2b8ebd396a558d71f73a3b
Mike Mandato <mike.mandato@verint.com>
2022-12-29 10:55:23 -0500

    Add null call back

--- app.js
+++ app.js
@@ -182,12 +182,12 @@ app.post('/webhook/', function (req, res) {
 
 
 app.get('/auth/facebook',
-  passport.authenticate('facebook', {scope: 'public_profile'}, () => {})
+  passport.authenticate('facebook', {scope: 'public_profile'}, null)
 )
 
 app.get('/auth/facebook/callback',
   passport.authenticate(
-    'facebook', {successRedirect: '/broadcast/broadcast', failureRedirect: '/broadcast'}, () => {}
+    'facebook', {successRedirect: '/broadcast/broadcast', failureRedirect: '/broadcast'}, null
   )
 )
 
